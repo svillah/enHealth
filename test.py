95427f90e94a623f1020260cbce74d679c1395c0
Sam <villaluzsam@gmail.com>
2017-02-11 00:55:59 -0500

    find some fields for future json template

--- test.py
+++ test.py
@@ -10,6 +10,44 @@ soup = BeautifulSoup(html, 'html.parser')
 for script in soup(["script", "style"]):
     script.extract()
 
-# sample outputs
-print(soup.h2.string)
-print(soup.find_all('a'))
+# finds generic drug name
+genericName = soup.h1.span.string
+print "Generic Name: " + genericName + "\n"
+
+# finds common brands 
+brandNames = soup.find("span", {"class":"comma-separated"})
+brandName = brandNames.find_all('a')
+
+print "Brand Names:" 
+for brand in brandNames:
+    print brand.string.rstrip(), #rstrip removes all new lines \n
+
+# finds drug description
+desc = soup.find("p",{"itemprop":"description"})
+print "\n \nGeneral Description: " + desc.string 
+
+# finds side effects of taking drug
+sideEffects = soup.find_all("tr", {"data-yah-key":"side_effect"})
+print "Side Effects: "
+
+for sideEffect in sideEffects:
+    lis = sideEffect.find("span", {"itemprop":"name"})
+    print lis.string
+
+# below are not implemented
+# RecommendedDosage TBD
+
+# OverTheCounter
+
+# Interactions
+
+# Side effects paragraph
+
+# Usage
+
+# Active ingredients
+
+# Warnings
+
+# Source
+
