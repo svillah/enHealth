65c8112666c5ff4a0c84123e910c95c69f157ca6
Sam <villaluzsam@gmail.com>
2017-02-20 03:08:41 -0500

    get urls and export to json

--- test.py
+++ test.py
@@ -1,53 +1,76 @@
 import urllib
+import json
+import requests
 from bs4 import BeautifulSoup
 
-# initializes html doc
-testfile = "gabapentin.html"
-html = urllib.urlopen(testfile).read()
-soup = BeautifulSoup(html, 'html.parser')
+f = open('drugData.json', 'a+')
 
-# removes js and css 
-for script in soup(["script", "style"]):
-    script.extract()
+def initializeSoup(url):
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    return soup
 
-# finds generic drug name
-genericName = soup.h1.span.string
-print "Generic Name: " + genericName + "\n"
+def getDrugData(url):
+    website = url
+    soup = initializeSoup(website)
 
-# finds common brands 
-brandNames = soup.find("span", {"class":"comma-separated"})
-brandName = brandNames.find_all('a')
+    # removes js and css
+    for script in soup(["script", "style"]):
+        script.extract()
 
-print "Brand Names:" 
-for brand in brandNames:
-    print brand.string.rstrip(), #rstrip removes all new lines \n
+    # finds generic drug name
+    genericName = soup.h1.span.string
+    f.write("\nGeneric Name: " + genericName + "\n")
 
-# finds drug description
-desc = soup.find("p",{"itemprop":"description"})
-print "\n \nGeneral Description: " + desc.string 
+    # finds common brands
+    brandNames = soup.find("span", {"class":"comma-separated"})
+    brandName = brandNames.find_all('a')
+    f.write("Brand Names:")
+    for brand in brandNames:
+        f.write(brand.string.rstrip(),) #rstrip removes all new lines \n
 
-# finds side effects of taking drug
-sideEffects = soup.find_all("tr", {"data-yah-key":"side_effect"})
-print "Side Effects: "
+    # finds drug description
+    desc = soup.find("p",{"itemprop":"description"})
+    f.write("\nGeneral Description: " + desc.string)
 
-for sideEffect in sideEffects:
-    lis = sideEffect.find("span", {"itemprop":"name"})
-    print lis.string
+    # finds side effects of taking drug
+    sideEffects = soup.find_all("tr", {"data-yah-key":"side_effect"})
+    f.write("Side Effects: ")
 
-# below are not implemented
-# RecommendedDosage TBD
+    for sideEffect in sideEffects:
+        lis = sideEffect.find("span", {"itemprop":"name"})
+        f.write(lis.string)
+    f.write("\n")
 
-# OverTheCounter
+    # below are not implemented
+    # RecommendedDosage TBD
 
-# Interactions
+    # OverTheCounter
 
-# Side effects paragraph
+    # Interactions
 
-# Usage
+    # Side effects paragraph
 
-# Active ingredients
+    # Usage
 
-# Warnings
+    # Active ingredients
 
-# Source
+    # Warnings
 
+    # Source
+
+# get URLs of all drugs, note that this is only page one
+website = "https://www.patientslikeme.com/treatments/browse?cat=1"
+soup = initializeSoup(website)
+drugTable = soup.find("table", {"id":"tbl-treatments"})
+drugs = drugTable.findChildren('tr')
+
+#for i in range(1, len(drugs)):
+for i in range(1, 3): # use code above for actual implementation, current for test
+    for drugUrl in drugs[i].findChildren("a"):
+        url = drugUrl.get("href")
+        fullUrl = "https://www.patientslikeme.com" + url
+        # go to URL for each drug and obtain info
+        getDrugData(fullUrl)
+
+f.close()
